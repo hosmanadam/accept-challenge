aeb0a057e0e0aacbb5c75b4909dd3fd28b1581e6
Adam Hosman <github@adamsinbox.com>
2019-08-19 14:20:54 +0200

    Add ability to keep last N videos in playlist

--- clickAllRemoveButtonsOnYoutubePlaylist.js
+++ clickAllRemoveButtonsOnYoutubePlaylist.js
@@ -1,7 +1,7 @@
 /* This script will click all the remove buttons on your YouTube playlist
 at a rate of 5/second. This is the fastest speed at which it's worked
 reliably for me, but you can try making it faster if you need to remove
-thousands of videos. Works as of 2019-08-17. */
+thousands of videos. Works as of 2019-08-19. */
 
 // 1. Open your playlist in Chrome or Firefox
 // 2. Click EDIT next to your profile picture
@@ -11,8 +11,9 @@ thousands of videos. Works as of 2019-08-17. */
 // 4. Paste this code in:
 
 const REMOVE_PER_SECOND = 5; // Tweak this number if you want to
+const KEEP_THE_LAST = 0; // Set this number to keep the last N videos
 let removeButtons = document.querySelectorAll(".pl-video-edit-remove");
-for (let i = 0; i < removeButtons.length; i++) {
+for (let i = 0; i < removeButtons.length-KEEP_THE_LAST; i++) {
     setTimeout(
         ()=>removeButtons[i].click(),
         1000/REMOVE_PER_SECOND * i
